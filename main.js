be6e66b568dc3ae4f9bc3f98080837aa99bca247
John Bain <johnknoxbain@gmail.com>
2020-04-20 15:05:47 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let bandNumber = 1
+
+const takeNumber = function (band) {
+    let output = `${bandNumber}. ${band}`;
+    bandNumber++;
+    return output;
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
